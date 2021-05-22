e47b227ab75ee2e90f03cd8f2b1afe8941aacf34
FoozBarakat <reachfooz@gmail.com>
2021-05-22 06:23:13 +0200

    music-app

---
+++ music/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-tabs': 0,
+		'max-len': 'off',
+		'linebreak-style': 'off',
+		indent: 'off',
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
+};
