54544d54a53910fcbb7279b49460e95f71aff356
FoozBarakat <reachfooz@gmail.com>
2021-05-23 20:29:21 +0200

    routes

--- music/src/components/Header.vue
+++ music/src/components/Header.vue
@@ -3,7 +3,12 @@
 	<header id="header" class="bg-gray-700">
 		<nav class="container mx-auto flex justify-start items-center py-5 px-4">
 			<!-- App Name -->
-			<a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+			<router-link
+				class="text-white font-bold uppercase text-2xl mr-4"
+				:to="{ name: 'home' }"
+				exact-active-class="no-active"
+				>Music</router-link
+			>
 
 			<div class="flex flex-grow items-center justify-end">
 				<!-- Primary Navigation -->
@@ -16,7 +21,9 @@
 					</li>
 					<template v-else>
 						<li>
-							<a class="px-2 text-white" href="#">Manage</a>
+							<router-link class="px-2 text-white" :to="{ name: 'manage' }"
+								>Manage</router-link
+							>
 						</li>
 						<li>
 							<a class="px-2 text-white" href="#" @click.prevent="signOut"
@@ -31,7 +38,7 @@
 </template>
 
 <script>
-import { mapMutations, mapState, mapActions } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 
 export default {
 	name: 'Header',
@@ -40,7 +47,13 @@ export default {
 	},
 	methods: {
 		...mapMutations(['toggleAuthModel']),
-		...mapActions(['signOut']),
+		signOut() {
+			this.$store.dispatch('signOut');
+
+			if (this.$route.meta.requiresAuth) {
+				this.$router.push({ name: 'home' });
+			}
+		},
 	},
 };
 </script>
--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,25 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import Manage from '@/views/Manage.vue';
+import store from '@/store';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+	{
+		name: 'home',
+		path: '/',
+		component: Home,
+	},
+	{
+		name: 'manage',
+		// alias: '/manage',
+		path: '/manage-music',
+		component: Manage,
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/manage',
+		redirect: { name: 'manage' },
+	},
+	{
+		path: '/:catcgAll(.*)*',
+		redirect: { name: 'home' },
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
+	linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+	console.log(from);
+
+	if (!to.matched.some((record) => record.meta.requiresAuth)) {
+		next();
+		return;
+	}
+
+	if (store.state.userLoggedIn) {
+		next();
+	} else {
+		next({ name: 'home' });
+	}
 });
 
 export default router;
