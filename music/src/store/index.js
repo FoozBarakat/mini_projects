a7f2905294907533c95fca657ec1c2c4929d32da
FoozBarakat <reachfooz@gmail.com>
2021-05-25 19:36:59 +0200

    playing music

--- music/src/store/index.js
+++ music/src/store/index.js
@@ -1,10 +1,17 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
+import { Howl } from 'howler';
+import helper from '@/includes/helper';
 
 export default createStore({
 	state: {
 		authModelShow: false,
 		userLoggedIn: false,
+		currentSong: {},
+		sound: {},
+		seek: '00:00',
+		duration: '00:00',
+		playerProgress: '0%',
 	},
 	mutations: {
 		toggleAuthModel: (state) => {
@@ -13,9 +20,28 @@ export default createStore({
 		toggleAuth: (state) => {
 			state.userLoggedIn = !state.userLoggedIn;
 		},
+		newSong(state, payload) {
+			state.currentSong = payload;
+			state.sound = new Howl({
+				src: [payload.url],
+				html5: true,
+			});
+		},
+		updatePosition(state) {
+			state.seek = helper.formateTime(state.sound.seek());
+			state.duration = helper.formateTime(state.sound.duration());
+			state.playerProgress = `${(state.sound.seek() / state.sound.duration()) *
+				100}%`;
+		},
 	},
 	getters: {
-		// authModelShow: (state) => state.authModelShow,
+		playing: (state) => {
+			if (state.sound.playing) {
+				return state.sound.playing();
+			}
+
+			return false;
+		},
 	},
 	actions: {
 		async register({ commit }, payload) {
@@ -51,6 +77,58 @@ export default createStore({
 			await auth.signOut();
 
 			commit('toggleAuth');
+		},
+		async newSong({ commit, state, dispatch }, payload) {
+			if (state.sound instanceof Howl) {
+				state.sound.unload();
+			}
+
+			commit('newSong', payload);
+
+			state.sound.play();
+
+			state.sound.on('play', () => {
+				requestAnimationFrame(() => {
+					dispatch('progress');
+				});
+			});
+		},
+		async toggleAudio({ state }) {
+			if (!state.sound.playing) {
+				return;
+			}
+
+			if (state.sound.playing()) {
+				state.sound.pause();
+			} else {
+				state.sound.play();
+			}
+		},
+		progress({ commit, state, dispatch }) {
+			commit('updatePosition');
+
+			if (state.sound.playing()) {
+				requestAnimationFrame(() => {
+					dispatch('progress');
+				});
+			}
+		},
+		updateSeek({ state, dispatch }, payload) {
+			if (!state.sound.playing()) {
+				return;
+			}
+
+			const { x, width } = payload.currentTarget.getBoundingClientRect();
+			// clientX reletive to the document not to the player
+			const clickX = payload.clientX - x;
+			const percentage = clickX / width;
+			const secounds = state.sound.duration() * percentage;
+
+			state.sound.seek(secounds);
+
+			state.sound.once('seek', () => {
+				dispatch('progress');
+			});
 		},
 	},
 });
